1eda23aaf4d92a641316ce268d36f2cb8abd446e
ianwu4025005556 <encoref23160@gmail.com>
2021-08-05 15:22:11 +0800

    -調整project1中container-fluid的錯誤 讓padding顯示正確 -add the right paths to nav in several pages -add the background to lower the visibility of html content when open the nav

--- project2.js
+++ project2.js
@@ -1,13 +1,21 @@
 var navBtn = document.getElementById("openNav");
-var display = document.getElementsByClassName("nav-smallscreen")[0];
+var display = document.getElementsByClassName("nav-smallscreen");
 var close  = document.getElementById("closeNav");
+//var outterClose = window.getComputedStyle(display[0],"::before");
+//var bodyscroll = document.body.getElementsByTagName('overflow');
 
 navBtn.addEventListener('click',function(){
-	display.style.display = "block";
+	display[0].style.display = "block";
+	document.body.style.overflow = "hidden";
 	event.preventDefault();
 },true)
 
 close.addEventListener('click',function(){
-	display.style.display = "none";
-
+	display[0].style.display = "none";
+	document.body.style.cssText = "overflow:visible;";
 },true)
+
+// outterClose.addEventListener('click',function(){
+// 	display[0].style.display = "none";
+// 	document.body.style.cssText = "overflow:visible;";
+// },true)
