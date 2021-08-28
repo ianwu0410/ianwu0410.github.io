2c9198f5f8b230e30793455a474450fb192e5184
ianwu4025005556 <encoref23160@gmail.com>
2021-08-28 15:43:56 +0800

    -changing S1,S4 video to shorter version to improve better experience when reading -turnning S1 video to autoplay

--- script.js
+++ script.js
@@ -6,42 +6,29 @@ var target = document.getElementById("research");
 
 var tabBut1 = document.getElementById("firstTab");
 var tabBut2 = document.getElementById("secondTab");
-var tabBut3 = document.getElementById("thirdTab");
+
 
 var imgDisplay1 =document.getElementById("user1");
 var imgDisplay2 =document.getElementById("user2");
-var imgDisplay3 =document.getElementById("pain");
 
 tabBut2.addEventListener('click',function(){
 	imgDisplay2.style.display = "block";
 	imgDisplay1.style.display = "none";
-	imgDisplay3.style.display = "none";
+	
 
 	tabBut2.style.cssText = "color: #2F5DED; border: 2px #2F5DED solid;";
 	tabBut1.style.cssText = "color: #1c1c1c33; border: 2px #1c1c1c33 solid;";
-	tabBut3.style.cssText = "color: #1c1c1c33; border: 2px #1c1c1c33 solid;";
 	
 	event.preventDefault();
 })
-tabBut3.addEventListener('click',function(){
-	imgDisplay3.style.display = "block";
-	imgDisplay1.style.display = "none";
-	imgDisplay2.style.display = "none";
 
-	tabBut3.style.cssText = "color: #2F5DED; border: 2px #2F5DED solid;";
-	tabBut2.style.cssText = "color: #1c1c1c33; border: 2px #1c1c1c33 solid;";
-	tabBut1.style.cssText = "color: #1c1c1c33; border: 2px #1c1c1c33 solid;";
-	
-	event.preventDefault();
-})
 tabBut1.addEventListener('click',function(){
 	imgDisplay1.style.display = "block";
 	imgDisplay2.style.display = "none";
-	imgDisplay3.style.display = "none";
+
 
 	tabBut1.style.cssText = "color: #2F5DED; border: 2px #2F5DED solid;";
 	tabBut2.style.cssText = "color: #1c1c1c33; border: 2px #1c1c1c33 solid;";
-	tabBut3.style.cssText = "color: #1c1c1c33; border: 2px #1c1c1c33 solid;";
 	
 	event.preventDefault();
 })
